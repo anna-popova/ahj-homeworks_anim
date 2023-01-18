6ed4c786cac55dcca5d323930b0a91bc8ebd0d62
Anna Popova <panasenko-anna.8@mail.ru>
2023-01-18 08:38:33 +0300

    fix eslint problems

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,8 @@ const conteiner = document.querySelector('.conteiner');
 const button = conteiner.querySelector('.button');
 const collapse = conteiner.querySelector('.collapse');
 
-
 button.addEventListener('click', (e) => {
 	e.preventDefault();
 
 	collapse.classList.toggle('show');
-})
+});
